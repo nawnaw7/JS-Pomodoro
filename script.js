c1dd67c76d45d037df5b6429f349906c1195e1a0
Nawnaw7 <sirbu.ioana90@gmail.com>
2017-11-03 11:52:23 +0100

    Js module pattern

---
+++ script.js
@@ -0,0 +1,18 @@
+var timerController = (function() {
+	//Code here
+})();
+
+
+
+
+var UIController = (function() {
+	//Code here
+})();
+
+
+
+
+var controller = (function(timerCtrl, UICtrl) {
+	//Code here
+
+})(timerController, UIController);
