30e7f6478b0a5ca10d2fef550cd7d3d43d0a47a7
Nawnaw7 <sirbu.ioana90@gmail.com>
2017-11-05 16:21:49 +0100

    updates

--- script.js
+++ script.js
@@ -1,18 +1,112 @@
+//////////////////////////////////////////////
+// TIMER CONTROLLER
+/////////////////////////////////////////////
 var timerController = (function() {
-	//Code here
+
+    var Session = function(sessWork, sessBreak) {
+        this.sessWork = sessWork;
+        this.sessBreak = sessBreak;
+    };
+
+    var workLength = 25;
+    var breakLength = 5;
+   
+    return {
+        sessionTime: function(value) {
+            if (value === 'addWork') {
+                workLength++;
+            } else if (value === 'subWork') {
+                workLength--;
+            } else if (value === 'addBrk') {
+                breakLength++;
+            } else if (value === 'subBrk') {
+                breakLength--;
+            }
+
+            console.log(workLength + ', ' + breakLength);
+        }
+    };
 })();
 
 
 
 
+//////////////////////////////////////////////
+// UI CONTROLLER
+/////////////////////////////////////////////
 var UIController = (function() {
-	//Code here
+	var DOMStrings = {
+		plusMinBtns: '.buttons',
+        workTime: '#work-time',
+        breakTime: '#break-time',
+        addWork: '#add-work',
+        subWork: '#sub-work',
+        addBreak: '#add-break',
+        subBreak: '#sub-break',
+	};
+    
+    var workTime = document.querySelector(DOMStrings.workTime);
+    var breakTime = document.querySelector(DOMStrings.breakTime);
+    var work = 25;
+    var brk = 5;
+
+    return {
+
+        updateWorkBrkUI: function(val) {
+            if (val === 'addWork') {
+                work++;
+                workTime.innerHTML = work;
+            } else if (val === 'subWork') {
+                work--;
+                workTime.innerHTML = work;
+
+            } else if (val === 'addBrk') {
+                brk++;
+                breakTime.innerHTML = brk;
+
+            } else if (val === 'subBrk') {
+                brk--;
+                breakTime.innerHTML = brk;
+
+            }
+        },
+
+    	getDOMStrings: function() {
+    		return DOMStrings;
+    	}
+	};
+
 })();
 
 
 
 
+
+
+//////////////////////////////////////////////
+// CONTROLLER
+/////////////////////////////////////////////
 var controller = (function(timerCtrl, UICtrl) {
-	//Code here
+
+	var DOM = UICtrl.getDOMStrings();
+	
+    document.querySelector(DOM.addWork).addEventListener('click', updateSessAndBreak);
+    document.querySelector(DOM.subWork).addEventListener('click', updateSessAndBreak);
+    document.querySelector(DOM.addBreak).addEventListener('click', updateSessAndBreak);
+    document.querySelector(DOM.subBreak).addEventListener('click', updateSessAndBreak);
+
+
+    function updateSessAndBreak() {
+        var newSession;
+
+        //1. Get value of buttons
+        var btnValue = this.value;
+
+        //2. Create session and break in Timer Controller
+        timerCtrl.sessionTime(btnValue);
+
+        //3. Update UI
+        UICtrl.updateWorkBrkUI(btnValue);
+    }
 
 })(timerController, UIController);
